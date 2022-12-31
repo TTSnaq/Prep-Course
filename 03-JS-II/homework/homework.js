ede5abca01a939f919de0f2574cb8e695f6c14de
TTSnaq <aamc2305A@gmail.com>
2022-12-30 22:36:31 -0400

    backup

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -195,17 +195,7 @@ return 'Soy falso'}
 function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
-  //Escribe tu código aquí  
-  
-  const Tabla6 = [];
-    
-  for (var i = 0; i <= 60; i+=6) {
-  
-    Tabla6.push(i);
-     
-    }
-     
-  console.log(Tabla6);   
+  //Escribe tu código aquí    
   
 }
 
